3355cb42d1f79a4f667ad1a85f04ed51c957ef33
Tanmay Pardhi <55151822+Tanmay-Pardhi@users.noreply.github.com>
2024-10-03 00:44:25 +0530

    Update Footer.tsx

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <p id="copyright">
-            Do not enter PII & GenAI can make mistakes, so verify information if necessary, Thanks for trying out my chatbot.
+            Do not enter PII and GenAI can make mistakes, so verify information if necessary, Thanks for trying out my chatbot.
         </p>
     )
 }

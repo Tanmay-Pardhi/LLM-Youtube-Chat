3ace79c38fff60ad4209a474b022741673cd95f1
Tanmay Pardhi <tanmaypardhi.26@gmail.com>
2024-10-03 15:30:57 -0700

    Updated libraries

--- client/src/aws-exports.tsx
+++ client/src/aws-exports.tsx
@@ -1,8 +1,8 @@
 const awsExports = {
     "aws_project_region": "eu-west-1",
     "aws_cognito_region": "ew-west-1",
-    "aws_user_pools_id": "eu-west-1_6voX8oh9Z",
-    "aws_user_pools_web_client_id": "45bupbq7vp3n32ipcgcpp4hga2",
+    "aws_user_pools_id": "eu-west-1_9wS6H1A6R",
+    "aws_user_pools_web_client_id": "2374ne232v5th9k2226hfq3o7l",
     "oauth": {}
 }
 
